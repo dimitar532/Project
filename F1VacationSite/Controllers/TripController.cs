dd6514f0c30f0a4ceea4bc5273275bf48f6c5bc9
Mitko <Mitko@DESKTOP-K2KTD0M>
2026-02-09 22:35:20 +0200

    TripController

---
+++ F1VacationSite/Controllers/TripController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F1VacationSite.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+         return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+         public IActionResult Create()
+        {
+            return View();
+        }
+         public IActionResult Edit(int id)
+        {
+            return View();
+        }
+         public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
