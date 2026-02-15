4f05b4fb87decd94bb00ca57ec939a73eb5521c9
Mitko <Mitko@DESKTOP-K2KTD0M>
2026-02-15 21:11:51 +0200

    Race Controler Created

---
+++ F1VacationSite/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F1VacationSite.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
