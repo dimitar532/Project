737c665433f1084bb7225637db7f4ffdeb97b4f2
Mitko <Mitko@DESKTOP-K2KTD0M>
2026-04-02 22:00:55 +0300

    User constants created

---
+++ F1VacationSite/Constants/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace F1VacationSite.Constants
+{
+    public static class UserConstants
+    {
+        public const int FirstNameMinLength = 2;
+        public const int FirstNameMaxLength = 50;
+        public const int LastNameMinLength = 2;
+        public const int LastNameMaxLength = 50;
+        public const int PasswordMinLength = 8;
+    }
+}
+
+
