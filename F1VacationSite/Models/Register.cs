eeed3e20d7f02cab9713463875b8ef7976367ae1
Mitko <Mitko@DESKTOP-K2KTD0M>
2026-04-06 22:27:23 +0300

    Register model created.

---
+++ F1VacationSite/Models/Register.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using F1VacationSite.Constants;
+
+namespace F1VacationSite.View_Models
+{
+    public class Register
+    {
+        [Required]
+        [StringLength(UserConstants.FirstNameMaxLength, MinimumLength = UserConstants.FirstNameMinLength)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserConstants.LastNameMaxLength, MinimumLength = UserConstants.LastNameMinLength)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = UserConstants.PasswordMinLength)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
