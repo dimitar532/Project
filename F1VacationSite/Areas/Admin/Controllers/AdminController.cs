2a4b425b639b5698fde1c8a143f6c5f2f11ff4ac
Mitko <Mitko@DESKTOP-K2KTD0M>
2026-04-14 22:38:44 +0300

    Admin login

---
+++ F1VacationSite/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1VacationSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
